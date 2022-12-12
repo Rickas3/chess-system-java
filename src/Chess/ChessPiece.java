f45d493a78ac672039ca1b689c63bb6a20c9399a
Ricardo Silva <rickas3@hotmail.com>
2022-12-12 15:52:32 -0300

    contator de movimentos de pecas

--- src/Chess/ChessPiece.java
+++ src/Chess/ChessPiece.java
@@ -7,6 +7,8 @@ import Boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
    private  Color color;
+   private int moveCount;
+
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -15,6 +17,15 @@ public abstract class ChessPiece extends Piece {
     //RETIRADO O SET PARA NAO ALTERAR A COR APENAS ACESSA-LA
     public Color getColor() {
         return color;
+    }
+    public int getMoveCount(){
+        return moveCount;
+    }
+    public void increaseMoveCount(){
+        moveCount ++;
+    }
+    public void decreaseMoveCount(){
+        moveCount --;
     }
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
