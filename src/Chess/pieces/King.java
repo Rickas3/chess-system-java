d904d654361a9cf2b4de46354ab9d82b845eaff4
Ricardo Silva <rickas3@hotmail.com>
2022-12-13 11:44:45 -0300

    Movimento especial Castling

--- src/Chess/pieces/King.java
+++ src/Chess/pieces/King.java
@@ -2,13 +2,17 @@ package Chess.pieces;
 
 import Boardgame.Board;
 import Boardgame.Position;
+import Chess.ChessMath;
 import Chess.ChessPiece;
 import Chess.Color;
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private ChessMath chessMath;
+
+    public King(Board board, Color color,ChessMath chessMath) {
         super(board, color);
+        this.chessMath = chessMath;
     }
 
     @Override
@@ -19,6 +23,10 @@ public class King extends ChessPiece {
     private boolean canMove(Position position){
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return  p == null || p.getColor() != getColor();
+    }
+    private boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
     @Override
     public boolean[][] possibleMoves() {
@@ -66,6 +74,30 @@ public class King extends ChessPiece {
         if (getBoard().positionExists(p) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
         }
+        //MOVIMENTO ESPECIAL CASTLING
+        if (getMoveCount() == 0 && !chessMath.getCheck()) {
+            //MOVIMENTO ESPECIAL CASTLING torre do lado do rei "PEQUENO"
+            Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+            if (testRookCastling(posT1)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+        }
+            if (getMoveCount() == 0 && !chessMath.getCheck()){
+                //MOVIMENTO ESPECIAL CASTLING torre do lado do rei "GRANDE"
+                Position posT2 = new Position(position.getRow(), position.getColumn() -4);
+                if(testRookCastling(posT2)){
+                    Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                    Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                    Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                    if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
+                        mat[position.getRow()][position.getColumn() - 2] = true;
+                    }
+                }
+            }
         return mat;
 
     }
